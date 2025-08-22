5be926bb96fcf4138373337e364bb0f47d09c00c
Xubai Wang <18016038327@189.cn>
2025-08-22 23:18:03 +0800

    chore: github action update akshare

--- packages/python/akshare/update.sh
+++ packages/python/akshare/update.sh
@@ -4,4 +4,4 @@ script_dir=$(dirname "$0")
 fetcher_file="$script_dir/fetcher.nix"
 
 echo "{ fetchFromGitHub }:" > $fetcher_file
-nurl "https://github.com/akfamily/akshare" >> $fetcher_file
+nix run nixpkgs#nurl -- "https://github.com/akfamily/akshare" >> $fetcher_file
