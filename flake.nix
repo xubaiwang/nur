78d596187494a5313550f2c747ad709452dcd7c3
Xubai Wang <18016038327@189.cn>
2025-11-11 21:53:20 +0800

    add ddddocr

--- flake.nix
+++ flake.nix
@@ -18,9 +18,37 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in
-      {
-        packages = pkgs.callPackage ./packages { };
+      rec {
+        # packages = pkgs.callPackage ./packages { };
         devShells = pkgs.callPackage ./dev-shells { };
+
+        packages.python3Packages.ddddocr =
+          let
+            ppkgs = pkgs.python3Packages;
+          in
+          ppkgs.buildPythonPackage rec {
+            pname = "ddddocr";
+            version = "1.5.6";
+            pyproject = true;
+
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              hash = "sha256-KDmpQL+r4C4yhO8/nSoDcpKqn2QfNVtDqbcL7Onhtz0=";
+            };
+
+            build-system = with ppkgs; [ setuptools ];
+
+            dependencies = with ppkgs; [
+              onnxruntime
+              onnx
+              pillow
+              numpy
+              opencv-python-headless
+              fastapi
+              uvicorn
+              pydantic
+            ];
+          };
       }
     ))
     // (flake-utils.lib.eachDefaultSystemPassThrough (
